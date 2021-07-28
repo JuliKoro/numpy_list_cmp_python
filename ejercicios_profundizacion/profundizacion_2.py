3bc9e73b5b1a3e2034ac1528009453999f9ecd79
Julián Koroluk <julian.koroluk@outlook.com>
2021-07-28 12:40:43 -0300

    Update ejercicios_profundizacion
    Completos y resueltos los ejercicios de profundización 2 y 3

--- ejercicios_profundizacion/profundizacion_2.py
+++ ejercicios_profundizacion/profundizacion_2.py
@@ -45,4 +45,6 @@ if __name__ == '__main__':
     # Se espera obtener:
     # ['Tamara', 'Juan', 'Alberto'......]
 
+    nombres_filtrados = [x for x in nombres if (x[0] in padron)]
+    print('Nombres filtrados según el padrón: ', nombres_filtrados)
     print("terminamos")
